5443e63933b358b9e01d9847151c2b15f4562ecd
Nenad Savovic <nenad_savovic@yahoo.com>
2022-04-06 05:21:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Loading = () => (
   <div className="border bg-white-100 shadow rounded-md p-12 max-w-sm w-full mx-auto">
     <div className="animate-pulse flex space-x-6">
       <div className="flex-1 space-y-4 py-2">
-        <div className="h-4 bg-purple-300 rounded w-3/4"></div>
+        <div className="h-4 bg-purple-200 rounded w-3/4"></div>
         <div className="space-y-2">
           <div className="h-4 bg-purple-300 rounded"></div>
           <div className="h-4 bg-purple-300 rounded w-5/6"></div>
